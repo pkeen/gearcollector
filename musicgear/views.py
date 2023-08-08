8226bac2b544356a20eeb39dea8182bcf132ac7b
pkeen <pkeen7@gmail.com>
2023-08-08 17:35:14 -0600

    lab 1 - build basic hardcoded gear list template and about

--- musicgear/views.py
+++ musicgear/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+gear = [
+    {'make': 'The Loar', 'model': 'LH-204 Brownstone', 'category': 'guitar'}
+]
+
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def my_gear_index(request):
+    return render(request, 'mygear.html', {
+        'gear': gear
+    })
+
